63ad32672a4ba7986f5accf3937288b507293d01
VAISHNAVI M <vaishnavim1194@gmail.com>
2024-07-24 09:54:05 +0530

    SDP

---
+++ Sample/myapp/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="h-screen w-screen flex justify-center items-center">
+      Hello world!
+    </h1>
+  )
+}
---
+++ proj/my-project/src/main.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
